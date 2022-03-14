73114d1d7ee59e3d283562fd02bb8fce9b5c243f
BuilderMARK <73698747+BuilderMARK@users.noreply.github.com>
2022-03-14 23:14:57 +0100

    Billeder til knapper sta indsættes

--- app/src/main/java/com/example/andpaklods/Items.java
+++ app/src/main/java/com/example/andpaklods/Items.java
@@ -7,47 +7,61 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class Items extends AppCompatActivity
-{
-        private ImageButton MentaBtn, VodkaRedbullBtn, Ol, Cola, Mokai;
+public class Items extends AppCompatActivity implements View.OnClickListener{
+    int Mokai = 0;
+    int cola = 0;
+    int Vodka = 0;
+    int ol =0;
+    int Menta = 0;
         @Override
         protected void onCreate (Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_items);
-            MentaBtn.findViewById(R.id.searchImageButtonMenta);
-            VodkaRedbullBtn.findViewById(R.id.searchImageButtonVodkaRedbull);
-            Ol.findViewById(R.id.searchImageButtonØl);
-            Cola.findViewById(R.id.searchImageButtonCola);
-            Mokai.findViewById(R.id.searchImageButtonMokai);
+            ImageButton button1 = findViewById(R.id.searchImageButtonMokai);
+            ImageButton button2 = findViewById(R.id.searchImageButtonCola);
+            ImageButton button3 = findViewById(R.id.searchImageButtonØl);
+            ImageButton button4 = findViewById(R.id.searchImageButtonMenta);
+            ImageButton button5 = findViewById(R.id.searchImageButtonVodkaRedbull);
+            button1.setOnClickListener(this);
+            button2.setOnClickListener(this);
+            button3.setOnClickListener(this);
+            button4.setOnClickListener(this);
+            button5.setOnClickListener(this);
 
-            Mokai.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String toast = "";
-                    switch (view.getId()) {
-                        case R.id.searchImageButtonCola:
-                            toast = "Cola";
-                            System.out.printf("Cola");
-                            break;
-                        case R.id.searchImageButtonMenta:
-                            toast = "Menta";
-                            System.out.printf("Menta");
-                            break;
-                        case R.id.searchImageButtonMokai:
-                            toast = "Mokai";
-                            System.out.printf("Mokai");
-                            break;
-                        case R.id.searchImageButtonVodkaRedbull:
-                            toast = "VodkaRedbull";
-                            System.out.printf("VodkaRedbull");
-                            break;
-                        case R.id.searchImageButtonØl:
-                            toast = "Øl";
-                            System.out.printf("Øl");
-                    }
-                }
 
-            });
-        }}
+
+        }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.searchImageButtonCola:
+                Toast.makeText(this, "Antal Cola "+cola,Toast.LENGTH_SHORT).show();
+                System.out.printf("Antal Cola" + cola );
+                cola++;
+                break;
+            case R.id.searchImageButtonMenta:
+                Menta++;
+                Toast.makeText(this, "Antal Menta " + Menta,Toast.LENGTH_SHORT).show();
+                System.out.printf("Menta");
+                break;
+            case R.id.searchImageButtonMokai:
+                Mokai++;
+                Toast.makeText(this, "Antal Mokai "+Mokai,Toast.LENGTH_SHORT).show();
+                System.out.printf("Mokai");
+                break;
+            case R.id.searchImageButtonVodkaRedbull:
+                Vodka++;
+                Toast.makeText(this, "Antal VodkaRedbull "+Vodka,Toast.LENGTH_SHORT).show();
+                System.out.printf("VodkaRedbull");
+                break;
+            case R.id.searchImageButtonØl:
+                ol++;
+                Toast.makeText(this, "antal ØL " + ol,Toast.LENGTH_SHORT).show();
+                System.out.printf("ØL");
+    }
+
+    }
+}
 
 
